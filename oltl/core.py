39152ff635672f3c102e4e090b610bffc101984b
osoken <osoken@osoekawalab.com>
2024-03-12 00:04:51 +0900

    feat(core): add Id class

--- oltl/core.py
+++ oltl/core.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Any, Type, TypeVar
+from typing import Any, Type, TypeVar, Union
 
-from pydantic import GetCoreSchemaHandler, TypeAdapter
+import ulid
+from pydantic import GetCoreSchemaHandler, GetJsonSchemaHandler, TypeAdapter
+from pydantic.json_schema import JsonSchemaValue
 from pydantic_core import core_schema
+from ulid import ULID
 
 StrT = TypeVar("StrT", bound="BaseString")
+IdT = TypeVar("IdT", bound="Id")
 
 
 class BaseString(str):
@@ -115,3 +119,58 @@ class LimitedMaxLengthMixin(ABC, BaseString):
     @classmethod
     def __get_extra_constraint_dict__(cls) -> dict[str, Any]:
         return super().__get_extra_constraint_dict__() | {"max_length": cls.get_max_length()}
+
+
+class Id(ULID):
+    r"""Id is a string type that can be used to validate and serialize ULID strings.
+
+    >>> Id(ulid.from_int(1))
+    Id('00000000000000000000000001')
+    >>> Id(b"\x01\x8e.\t\xa9\x06=\x9b\x0fK\xaa\xdc'\x01\xe0;")
+    Id('01HRQ0KA867PDGYJXAVGKG3R1V')
+    >>> Id("01HRQ0BNKS4WMFVQPW810MPM3V")
+    Id('01HRQ0BNKS4WMFVQPW810MPM3V')
+    >>> Id("01HRQ0BNKS4WMFVQPW810MP")
+    Traceback (most recent call last):
+     ...
+    ValueError: Invalid ULID string: 01HRQ0BNKS4WMFVQPW810MP
+    """
+
+    def __init__(self, value: Union[str, ULID, bytes]) -> None:
+        if isinstance(value, ULID):
+            super(Id, self).__init__(value.bytes)
+            return
+        if isinstance(value, bytes):
+            super(Id, self).__init__(value)
+            return
+        if len(value) != 26:
+            raise ValueError(f"Invalid ULID string: {value}")
+        super(Id, self).__init__(ulid.base32.decode_ulid(value))
+
+    @classmethod
+    def generate(cls: Type[IdT]) -> IdT:
+        return cls(ulid.new())
+
+    def serialize(self) -> str:
+        return str(self)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.str}')"
+
+    @classmethod
+    def validate(cls: Type[IdT], value: Any) -> IdT:
+        if isinstance(value, cls):
+            return value
+        if isinstance(value, (ULID, str)):
+            return cls(value)
+        raise ValueError(f"Cannot convert {value} to {cls}")
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type: Any, _handler: GetCoreSchemaHandler) -> core_schema.CoreSchema:
+        return core_schema.no_info_plain_validator_function(
+            cls.validate,
+            serialization=core_schema.plain_serializer_function_ser_schema(cls.serialize, when_used="json"),
+        )
+
+    def __get_pydantic_json_schema__(self, _handler: GetJsonSchemaHandler) -> JsonSchemaValue:
+        return {"format": "crockfordBase32", "type": "string"}
