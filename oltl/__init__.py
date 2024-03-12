e8feb6c2fa777af517bea10efd854b39d2a58dd2
osoken <osoken@osoekawalab.com>
2024-03-12 21:29:02 +0900

    feat(package): export Id

--- oltl/__init__.py
+++ oltl/__init__.py
@@ -1,1 +1,5 @@
+from .core import Id
+
 __version__ = "0.0.1"
+
+__all__ = ["Id", "Timestamp"]
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,7 +1,14 @@
 import re
 
+import oltl
+
 
 def test_oltl_has_version() -> None:
-    import oltl
 
     assert re.match(r"\d+\.\d+\.\d+", oltl.__version__)
+
+
+def test_oltl_has_id() -> None:
+    expected = "01HRQ0BNKS4WMFVQPW810MPM3V"
+    actual = oltl.Id("01HRQ0BNKS4WMFVQPW810MPM3V")
+    assert actual.str == expected
