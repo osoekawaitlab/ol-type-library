6f2132aea628b856392a63b328318aca377981e8
osoken <osoken@osoekawalab.com>
2024-03-13 21:17:26 +0900

    feat(core): add BaseModel

--- oltl/core.py
+++ oltl/core.py
@@ -1,17 +1,20 @@
 from abc import ABC, abstractmethod
 from datetime import datetime as _datetime
 from datetime import timezone as _timezone
-from typing import Any, Type, TypeVar, Union
+from typing import Any, Type, TypeAlias, TypeVar, Union
 
 import ulid
 from dateutil.parser import parse as parse_datetime
-from pydantic import GetCoreSchemaHandler, GetJsonSchemaHandler, TypeAdapter
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import ConfigDict, GetCoreSchemaHandler, GetJsonSchemaHandler, TypeAdapter
+from pydantic.alias_generators import to_camel
 from pydantic.json_schema import JsonSchemaValue
 from pydantic_core import core_schema
 from ulid import ULID
 
 StrT = TypeVar("StrT", bound="BaseString")
 IdT = TypeVar("IdT", bound="Id")
+IncEx: TypeAlias = "set[int] | set[str] | dict[int, IncEx] | dict[str, IncEx] | None"
 
 
 class BaseString(str):
@@ -294,3 +297,53 @@ class Timestamp(int):
         1674397764479000
         """
         return int(self)
+
+
+class BaseModel(PydanticBaseModel):
+    """
+    >>> from unittest.mock import patch
+    >>> from datetime import timedelta, timezone, datetime
+    >>> from pydantic import Field
+    >>> class DerivedModel(BaseModel):
+    ...   object_name: str
+    >>> x = DerivedModel(object_name='test')
+    >>> x
+    DerivedModel(object_name='test')
+    >>> x.model_dump()
+    {'object_name': 'test'}
+    >>> x.model_dump_json()
+    '{"objectName":"test"}'
+    >>> DerivedModel.model_validate_json('{"objectName":"test2"}')
+    DerivedModel(object_name='test2')
+    >>> DerivedModel.model_validate_json('{"object_name":"test3"}')
+    DerivedModel(object_name='test3')
+    >>> DerivedModel.model_json_schema()
+    {'properties': {'objectName': {'title': 'Objectname', 'type': 'string'}}, 'required': ['objectName'], 'title': 'DerivedModel', 'type': 'object'}
+    """  # noqa: E501
+
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, validate_assignment=True)
+
+    def model_dump_json(
+        self,
+        *,
+        indent: int | None = None,
+        include: IncEx = None,
+        exclude: IncEx = None,
+        by_alias: bool = True,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+        round_trip: bool = False,
+        warnings: bool = True,
+    ) -> str:
+        return super(BaseModel, self).model_dump_json(
+            indent=indent,
+            include=include,
+            exclude=exclude,
+            by_alias=by_alias,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+            round_trip=round_trip,
+            warnings=warnings,
+        )
