ecc7dffcbc52511d65f42febd095bf817ef54915
osoken <osoken@osoekawalab.com>
2024-03-15 23:39:17 +0900

    test(core): add test for immutable fields

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
 
+import pytest
 from freezegun import freeze_time
+from pydantic import ValidationError
 from pytest_mock import MockerFixture
 from ulid import ULID
 
@@ -46,6 +48,17 @@ def test_derived_entity_has_derived_id(mocker: MockerFixture) -> None:
     assert actual2 == expected2
 
 
+def test_entity_id_is_immutable() -> None:
+    class MyId(core.Id): ...
+
+    class MyEntity(core.BaseEntity[MyId]):
+        name: str
+
+    entity = MyEntity(id=MyId("01HRQ0KA867PDGYJXAVGKG3R1V"), name="foo")
+    with pytest.raises(ValidationError, match="1 validation error for MyEntity\nid\n\\s+Field is frozen.*"):
+        entity.id = MyId("01HRQ0KA867PDGYJXAVGKG3R1V")
+
+
 def test_derived_entity_serialize_deserialize() -> None:
     class MyId(core.Id): ...
 
@@ -58,3 +71,33 @@ def test_derived_entity_serialize_deserialize() -> None:
     assert actual == serialize_expected
     deserialize_expected = entity
     assert MyEntity.model_validate_json(actual) == deserialize_expected
+
+
+def test_creation_time_aware_model_has_created() -> None:
+    class MyModel(core.BaseCreationTimeAwareModel):
+        object_name: str
+        some_value: int
+
+    dt = datetime(2024, 3, 15, 23, 31, 21, 123456, tzinfo=timezone.utc)
+    with freeze_time(dt):
+        actual = MyModel(object_name="foo", some_value=42)
+    expected = MyModel(created_at=core.Timestamp(dt), object_name="foo", some_value=42)
+    assert actual == expected
+
+    dt2 = datetime(2024, 3, 15, 23, 33, 15, 123456, tzinfo=timezone.utc)
+    with freeze_time(dt2):
+        actual2 = MyModel(object_name="bar", some_value=99)
+    expected2 = MyModel(created_at=core.Timestamp(dt2), object_name="bar", some_value=99)
+    assert actual2 == expected2
+
+
+def test_created_at_is_immutable() -> None:
+    class MyModel(core.BaseCreationTimeAwareModel):
+        object_name: str
+        some_value: int
+
+    dt = datetime(2024, 3, 15, 23, 31, 21, 123456, tzinfo=timezone.utc)
+    with freeze_time(dt):
+        model = MyModel(object_name="foo", some_value=42)
+    with pytest.raises(ValidationError, match="1 validation error for MyModel\ncreated_at\n\\s+Field is frozen.*"):
+        model.created_at = core.Timestamp.now()
