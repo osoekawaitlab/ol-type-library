008acb2f76412abf48b88b7647b030351b22cc8e
osoken <osoken@osoekawalab.com>
2024-03-16 10:54:58 +0900

    feat(settings): add settings file

---
+++ oltl/settings.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings as PydanticBaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class BaseSettings(PydanticBaseSettings):
+    model_config = SettingsConfigDict(env_file_encoding="utf-8", env_nested_delimiter="__")
