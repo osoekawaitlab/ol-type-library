7746c3ef6c8f5f5875d76e8a5628f6db160734c9
osoken <osoken@osoekawalab.com>
2024-03-23 01:41:28 +0900

    test(package): make tests simple

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,9 +1,7 @@
 import re
-from datetime import datetime
-
-from freezegun import freeze_time
 
 import oltl
+from oltl import core
 
 
 def test_oltl_has_version() -> None:
@@ -12,57 +10,24 @@ def test_oltl_has_version() -> None:
 
 
 def test_oltl_has_id() -> None:
-    expected = "01HRQ0BNKS4WMFVQPW810MPM3V"
-    actual = oltl.Id("01HRQ0BNKS4WMFVQPW810MPM3V")
-    assert actual.str == expected
+    oltl.Id == core.Id
 
 
 def test_oltl_has_timestamp() -> None:
-    expected = 1627574400000000
-    actual = oltl.Timestamp("2021-07-29T16:00:00Z")
-    assert actual == expected
+    oltl.Timestamp == core.Timestamp
 
 
 def test_oltl_has_base_model() -> None:
-    class MyModel(oltl.BaseModel):
-        object_name: str
-        some_value: int
-
-    actual = MyModel(object_name="foo", some_value=42)
-    expected = '{"objectName":"foo","someValue":42}'
-    assert actual.model_dump_json() == expected
+    oltl.BaseModel == core.BaseModel
 
 
 def test_oltl_has_base_entity() -> None:
-    class MyId(oltl.Id): ...
-
-    class MyEntity(oltl.BaseEntity[MyId]):
-        entity_name: str
-
-    entity = MyEntity(id=MyId("01HRQ0KA867PDGYJXAVGKG3R1V"), entity_name="foo")
-    actual = entity.model_dump_json()
-    expected = '{"id":"01HRQ0KA867PDGYJXAVGKG3R1V","entityName":"foo"}'
-    assert actual == expected
-    assert MyEntity.model_validate_json(actual) == entity
+    oltl.BaseEntity == core.BaseEntity
 
 
 def test_oltl_has_base_creation_time_aware_model() -> None:
-    class MyModel(oltl.BaseCreationTimeAwareModel):
-        object_name: str
-        some_value: int
-
-    with freeze_time(datetime(2024, 3, 23, 1, 22, 24, 123456)):
-        actual = MyModel(object_name="foo", some_value=42)
-        expected = '{"createdAt":1711156944123456,"objectName":"foo","someValue":42}'
-        assert actual.model_dump_json() == expected
+    oltl.BaseCreationTimeAwareModel == core.BaseCreationTimeAwareModel
 
 
 def test_oltl_has_base_update_time_aware_model() -> None:
-    class MyModel(oltl.BaseUpdateTimeAwareModel):
-        object_name: str
-        some_value: int
-
-    with freeze_time(datetime(2024, 3, 23, 1, 25, 9, 123456)):
-        actual = MyModel(object_name="foo", some_value=42)
-        expected = '{"createdAt":1711157109123456,"updatedAt":1711157109123456,"objectName":"foo","someValue":42}'
-        assert actual.model_dump_json() == expected
+    oltl.BaseCreationTimeAwareModel == core.BaseCreationTimeAwareModel
