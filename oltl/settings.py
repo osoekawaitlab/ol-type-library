5aee41395dc151564818a0b735e62f4ebf205973
osoken <osoken@osoekawalab.com>
2024-07-11 21:40:28 +0900

    feat: add loading settings from JSON file

--- oltl/settings.py
+++ oltl/settings.py
@@ -1,5 +1,14 @@
+from typing import Optional, Type, TypeVar
+
+from pydantic import Field, FilePath
 from pydantic_settings import BaseSettings as PydanticBaseSettings
-from pydantic_settings import SettingsConfigDict
+from pydantic_settings import (
+    JsonConfigSettingsSource,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
+
+SettingsClassT = TypeVar("SettingsClassT", bound="BaseSettings")
 
 
 class BaseSettings(PydanticBaseSettings):
@@ -9,7 +18,6 @@ class BaseSettings(PydanticBaseSettings):
     The child classes will support setting values via environment variables even nested ones.
 
 
-    >>> from oltl.settings import BaseSettings
     >>> class SomeSettings(BaseSettings):
     ...     some_value: str
     ...
@@ -22,6 +30,32 @@ class BaseSettings(PydanticBaseSettings):
     ...     settings = SomeNestedSettings()
     >>> settings.some_nested_value.some_value
     'value'
+    >>> settings.model_dump_json()
+    '{"some_nested_value":{"some_value":"value"}}'
     """
 
-    model_config = SettingsConfigDict(env_file_encoding="utf-8", env_nested_delimiter="__")
+    model_config = SettingsConfigDict(env_file_encoding="utf-8", env_nested_delimiter="__", frozen=False)
+
+    config_path: Optional[FilePath] = Field(default=None, exclude=True)
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[PydanticBaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            JsonConfigSettingsSource(settings_cls),
+        )
+
+
+def load_settings(setting_cls: Type[SettingsClassT], json_file_path: str) -> SettingsClassT:
+    setting_cls.model_config["json_file"] = json_file_path
+    return setting_cls()
